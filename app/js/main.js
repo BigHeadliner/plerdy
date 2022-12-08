5c12ea5bfe7fb65cf0b21ff09b950955cf75a978
BigHeadliner <bigheadliner@gmail.com>
2022-12-08 16:18:37 +0200

    landing page is ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function () { 
      
-   
+     
+    $('.reasons__slider').slick({ 
+		prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="images/prev.arrow.svg"></button>',  
+	    nextArrow:'<button type="button" class="slick-btn slick-next"><img src="images/next.arrow.svg"></button>', 
+        slidesToShow: 4,
+        slidesToScroll: 1, 
+       
+	});  
 
 });
