1bf5ceefa3313095b6fa464ee4d19a21a2f24896
BigHeadliner <bigheadliner@gmail.com>
2022-12-10 00:39:56 +0200

    popup form ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,78 @@
 $(function () { 
-     
+      
+     // const hamburger = document.getElementById('menu__btn');
+    // const headerMenu = document.getElementById('menu__list');
+    // const menuLink = document.querySelectorAll('.menu__link');
+    const popUpBody = document.getElementById('popup');
+    const openPopUp = document.querySelectorAll('.popup-open-btn');
+    const closePopUp = document.getElementById('close-popup');  
+    const form = document.getElementById('form');   
+    
+      
+    
+
      
     $('.reasons__slider').slick({ 
 		prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="images/prev.arrow.svg"></button>',  
 	    nextArrow:'<button type="button" class="slick-btn slick-next"><img src="images/next.arrow.svg"></button>', 
         slidesToShow: 4,
         slidesToScroll: 1, 
+	});   
+     
+   
+  
+  
+    // hamburger.addEventListener('click', () => {
+    //   headerMenu.classList.toggle('menu__list--active');
+    //   hamburger.classList.toggle('menu__btn--open');
+    // })
+  
+    // menuLink.forEach(link => {
+    //   link.addEventListener('click', () => {
+    //     headerMenu.classList.remove('menu__list--active');
+    //     hamburger.classList.remove('menu__btn--open');
+    //   })
+    // })
+  
+    openPopUp.forEach(link => {
+        link.addEventListener('click', () => {
+          popUpBody.classList.add('active-popup');
+        })
+      })
+    
+    
+    
+      closePopUp.addEventListener('click', () => {
+        popUpBody.classList.remove('active-popup');
+      }) 
+         
        
-	});  
+      var input = document.querySelector("#phone");
+     window.intlTelInput(input); 
+
+      
+       $("#form").validate({ 
+          rules: {  
+             phone:{ 
+               required:true,    
+            //  // phone:true, 
+             
+             },
+            name:{ 
+              required: true, 
+              minlength: 3
+            },
+          }, 
+          messages:{ 
+            phone:{ 
+               required:'Error'
+             },  
+             
+            name:{ 
+              required:'Error'
+            },
+          }
+       }); 
+     
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,9 @@ function nunjucks(){
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js', 
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',  
     'node_modules/counterup2/dist/index.js', 
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',   
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',    
+    'node_modules/intl-tel-input/build/js/intlTelInput.js',  
+    'node_modules/jquery-validation/dist/jquery.validate.js', 
     'app/js/main.js'
    ]) 
    .pipe(concat('main.min.js')) 
